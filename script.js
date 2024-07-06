753dc3446f93e4f9415338a21caa9c872d6bd435
asifkhansafeer <asifkhantfm555@gmail.com>
2024-07-06 19:57:47 +0500

    update

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+/* programe for Table
+
 var table = +prompt("Enter you value");
 for(var i = 1; i <= 10; i++){
     console.log(`${table} * ${i} = ${table * i}`)
 }
+*/
+
+/*   programe for adding 5 values of array
+
+var array = [];
+var sum = 0;
+for (var i = 0; i < 5; i++){
+    var input = +prompt("Enter Your values");
+    array.push(input)
+    sum = sum + input;
+}
+console.log(`your 5 values of sum is: ${sum}`)
+*/
